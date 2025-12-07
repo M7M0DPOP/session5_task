74371ed9299c7a730e930408c495508e7e12f951
M7M0D <mg6607003@gmail.com>
2025-12-07 18:49:25 +0200

    nah

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:session5_task/features/authentication/presentation/pages/login_page.dart';
 import 'package:session5_task/features/authentication/presentation/pages/register_page.dart';
 
 UserCredential? userCredential;
