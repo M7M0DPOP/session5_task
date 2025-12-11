1106880e74d855795fbd247de43fe17ef6c733f4
M7M0D <mg6607003@gmail.com>
2025-12-11 13:24:28 +0200

    -default options

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:session5_task/features/authentication/presentation/pages/register_page.dart';
+import 'package:session5_task/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MaterialApp(home: RegisterPage()));
 }
